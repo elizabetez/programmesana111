a1cc51e5f34a5fa7ff21c283f3208cf51b910d08
elizabetez <149487046+elizabetez@users.noreply.github.com>
2024-03-27 13:15:22 +0200

    27

--- trenins.py
+++ trenins.py
@@ -11,15 +11,33 @@ for skaitlis in range(1, 11, 2): #range - funkcija, kas skaita skaitļus
         print("pirmais skaitlis:", skaitlis,"otrais skaitlis:", otrs, "rezultāts:", rezultats (skaitlis, otrs))
 
 
-def zvaigznites (skaits):
-    for rindasNr in range (1, skaits+1):
-        for zvaigznite in range (rindasNr):
-        print ("*", end="")
-    print("")
+def zvaigznites(skaits):
+    for rindasNr in range(1, skaits+1):
+         for zvaigzne in range(rindasNr):
+             print("*",end="")
+        print("")
 
     def zvaigznites2(skaits):
+        for rindasNr in range(1, skaits+1):
+            print("*"*rindasNr)
         
 zvaigznites (7)
+
+
+
+
+saraksts1 = [1, 7, 5, 9, 35, 2]
+saraksts2 = [4, 2, 2, 39, 6, 4]
+for skaititajs in range(len(saraksts1)):
+    print("skaititajs:", skaititajs, "pirmais skaitlis:", saraksts1[skaititajs],"otrais skaitlis:", saraksts2[skaititajs], "rezultāts:", rezultats (saraksts1[skaititajs], saraksts2[skaititajs]))
+
+
+
+
+
+
+
+
 
 skaitlis1 = 7
 skaitlis2 = 3
