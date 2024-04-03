7f1b69dde65f179bf3a1334895c9b85ddf5c1df9
elizabetez <149487046+elizabetez@users.noreply.github.com>
2024-04-03 12:44:16 +0300

    332

--- main.py
+++ main.py
@@ -10,6 +10,13 @@ def index():
 def te():
     return render_template("teksts.html")
 
+@app.route("/saraksts")
+def saraksts():
+    saraksts = ["Anna", "Katls", "Kartupelis"]
+    bildes = ["https://artprojectsforkids.org/wp-content/uploads/2022/05/How-to-Draw-a-Girl.jpg","https://dans.lv/imgs/1gb-nami%C5%86%C5%A1-mini-roku-mazg%C4%81%C5%A1anas-pot-miniat%C5%ABras_1-image/143144.jpg","https://stemgeneration.org/wp-content/uploads/2018/03/Potato_Battery_Main.jpg"]
+    kopejais_saraksts = [["Anna", "https://artprojectsforkids.org/wp-content/uploads/2022/05/How-to-Draw-a-Girl.jpg"], ["Katls", "https://dans.lv/imgs/1gb-nami%C5%86%C5%A1-mini-roku-mazg%C4%81%C5%A1anas-pot-miniat%C5%ABras_1-image/143144.jpg"], ["Kartupelis", "https://stemgeneration.org/wp-content/uploads/2018/03/Potato_Battery_Main.jpg"]]
+    return render_template("saraksts.html", vardi = saraksts, bildes = bildes, garums = len(saraksts))
+
 
 if __name__ == '__main__':
     app.run(port = 5000)
--- trenins.py
+++ trenins.py
@@ -6,22 +6,23 @@ def rezultats(sk1, sk2):
 
     return rez
 
-for skaitlis in range(1, 11, 2): #range - funkcija, kas skaita skaitļus
-    for otrs in range (2, 11, 2):
-        print("pirmais skaitlis:", skaitlis,"otrais skaitlis:", otrs, "rezultāts:", rezultats (skaitlis, otrs))
+for skaitlis in range(1, 11, 2):        #range - funkcija, kas skaita skaitļus
+    for otrs in range(2, 11, 2):
+        print("pirmais skaitlis:", skaitlis,"otrais skaitlis:", otrs, "rezultāts: ", rezultats(skaitlis, otrs))
 
 
-def zvaigznites(skaits):
+def zvaigznites1(skaits):
     for rindasNr in range(1, skaits+1):
-         for zvaigzne in range(rindasNr):
-             print("*",end="")
+        for zvaigzne in range(rindasNr):
+            print("*", end="")
         print("")
 
-    def zvaigznites2(skaits):
-        for rindasNr in range(1, skaits+1):
-            print("*"*rindasNr)
-        
-zvaigznites (7)
+def zvaigznites2(skaits):
+    for rindasNr in range(1, skaits+1):
+        print("*"*rindasNr)
+
+
+zvaigznites1(7)
 
 
 
@@ -30,10 +31,6 @@ saraksts1 = [1, 7, 5, 9, 35, 2]
 saraksts2 = [4, 2, 2, 39, 6, 4]
 for skaititajs in range(len(saraksts1)):
     print("skaititajs:", skaititajs, "pirmais skaitlis:", saraksts1[skaititajs],"otrais skaitlis:", saraksts2[skaititajs], "rezultāts:", rezultats (saraksts1[skaititajs], saraksts2[skaititajs]))
-
-
-
-
 
 
 
@@ -52,3 +49,5 @@ print(pirmais)
 vards2 = "Nē"
 
 print(pirmais + vards2)
+
+
